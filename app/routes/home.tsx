30bf930cc3865b35fc0b90eadc7d283830cfc829
ericsinghh <singheric890@gmail.com>
2026-07-13 01:49:07 +0530

    React Router project with application structure, UI assets, and container configuration

---
+++ app/routes/home.tsx
@@ -0,0 +1,22 @@
+import type { Route } from "./+types/home";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "content feedback for your dream job" },
+  ];
+}
+
+export default function Home() {
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+
+  </main>
+}
